8d3791c05bc99dda40cbb7e40d955b26cdcdedb1
Maycon <maycon.wb.santos@gmail.com>
2022-12-06 23:37:25 -0300

    Commented layout, added login and register

---
+++ Watermetter/Controllers/CadastroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Watermetter.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
