4f3da449659ee7c1e0e4c18d7bfb7cd6c7c1cc5e
Joao Bernardi <bernardi.joao0407@gmail.com>
2022-12-12 04:46:42 -0300

    Alterado o estilo e adicionado o dashboard

---
+++ Watermetter/Models/Calcs.cs
@@ -0,0 +1,9 @@
+﻿using Watermetter.Models;
+
+public class Calcs
+{
+    public double AverageConsumeByDay { get; set; }
+    public double AverageConsumeByWeek { get; set; }
+    public double AverageConsumeByMonth { get; set; }
+    public double AverageCostPerMonth { get; set; }
+}
