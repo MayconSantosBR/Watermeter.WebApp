68620b6c87777bd2b195ad92073614323bdad61c
Joao Bernardi <bernardi.joao0407@gmail.com>
2022-12-12 00:13:33 -0300

    Alteração historico

---
+++ Watermetter/Models/History.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Watermetter.Models
+{
+    public class History
+    {
+        public int IdArduino { get; set; }
+        public string IdOwner { get; set; }
+        public DateTime? Begin { get; set; }
+        public DateTime? End { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Waterflow { get; set; }
+
+    }
+}
